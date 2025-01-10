5e82a2dfe4439d2ae1a985537bcc9b154c72413b
shensyuan <sxs940302@gmail.com>
2025-01-10 09:57:13 +0800

    temp

--- Assets/Scripts/EnterGame.cs
+++ Assets/Scripts/EnterGame.cs
@@ -32,7 +32,7 @@ public class LevelTrigger : MonoBehaviour
     private void Update()
     {
         // 玩家在觸發區，且按下 "E" 鍵
-        if (isPlayerInZone && Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (isPlayerInZone && Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log($"切換到關卡: {levelName}");
             SceneManager.LoadScene(levelName); // 切換場景
